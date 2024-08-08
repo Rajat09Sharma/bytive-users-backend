04f7c6860dfdd91e9493b27778c61f0fa163eb7a
Rajat09Sharma <rajat9.lyfe@gmail.com>
2024-08-08 22:38:24 +0530

    allow origin

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const app= express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST,DELETE,PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+  
 
 connectToMongoDB(process.env.URL).then(()=>console.log("MongoDB connect successfully."));
 
